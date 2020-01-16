f90eb505a4e6ce4627348ded98b51a22300d5cfc
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-16 02:20:46 +0100

    creating console app project

---
+++ NBPcurrency/NBPcurrencyConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using NBPcurrency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBPcurrencyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2000, 1, 2);
+            Currency currency = new Currency("CHF", date, date);
+        }
+    }
+}
